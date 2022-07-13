cb9415ba1f7f2c18c0ada51f8cf8e1d7b841cce1
AnhLe3698 <anhviet.le00@hotmail.com>
2022-07-13 13:54:36 -0600

    first file

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😁🤪✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤢🤮❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 2);
