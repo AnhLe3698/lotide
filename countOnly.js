237371949d11770b46c79850ebe1a2ad87850531
AnhLe3698 <anhviet.le00@hotmail.com>
2022-07-14 12:08:28 -0600

    Adding object counting function

--- countOnly.js
+++ countOnly.js
@@ -17,7 +17,7 @@ const countOnly = function(allItems, itemsToCount) {
       if (allItems[i] === item) {
         counter ++;
       }
-    } //The following condtional will add the key:value to 
+    } // The following condtional will add the key:value to
     // object if and only if the value is greater than zero
     // and the value of the key is true
     if (itemsToCount[`${item}`] && counter !== 0) {
