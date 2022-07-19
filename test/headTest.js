63e6f6414a259eeb5822ec04f3ab87f95a5a4412
AnhLe3698 <anhviet.le00@hotmail.com>
2022-07-19 16:09:32 -0600

    adding mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,8 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
