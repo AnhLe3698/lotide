d31ced79fbdaa896b0a329c4ee39416469e7a58d
AnhLe3698 <anhviet.le00@hotmail.com>
2022-07-18 22:42:44 -0600

    refactored tail

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😁🤪✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤢🤮❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let holder = [];
   for (let i = 1; i < array.length; i++) {
@@ -14,7 +6,4 @@ const tail = function(array) {
   return holder;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words).length, 3);
+module.exports = tail;
