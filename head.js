cb76a6c73450a224de44d40c1ec43de4dbdbed85
AnhLe3698 <anhviet.le00@hotmail.com>
2022-07-18 22:34:01 -0600

    refactored code

--- head.js
+++ head.js
@@ -1,15 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😁🤪✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤢🤮❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   return array[0];
 };
 
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('../assertEqual');
+assertEqual("", "");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+//assertEqual("3", 3);
